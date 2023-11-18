2cf07ccdf9453bfdc75ddea00fec1aa6c7c3cc6a
AlesLearningCode <146716296+AlesLearningCode@users.noreply.github.com>
2023-11-18 20:13:43 +0100

    Start work on projects
    Finishind array methods for searching for the right project so user
    can input their ToDos

--- src/DOM.js
+++ src/DOM.js
@@ -1,11 +1,12 @@
 import toDo from "./newToDo";
 import projects from "./newProject";
+import { get } from "lodash";
 const createBtn = document.querySelector(`.createToDo`)
 const title = document.querySelector(`.title`)
 const description = document.querySelector(`.description`)
 const date = document.querySelector(`.date`)
 createBtn.addEventListener(`click`, () =>{
-    projects[0].push(new toDo(title.value, description.value, "", date.value))
+    projects[projects.length - 1].push(new toDo(title.value, description.value, "", date.value))
     console.log(projects)
     loadToDos()
 })
@@ -28,7 +29,11 @@ const projectList = document.createElement(`datalist`)
 const projectInput = document.createElement(`input`)
 const content = document.querySelector(`#content`)
 projectInput.setAttribute(`list`,`projectList`)
+projectInput.classList.add(`projectInput`)
 projectList.setAttribute(`id`,`projectList`)
+const defaultProject = document.createElement(`option`)
+defaultProject.value = "default project"
+projectList.appendChild(defaultProject)
 document.body.appendChild(projectInput)
 document.body.appendChild(projectList)
 const projectName = document.querySelector(`.projectName`)  
@@ -36,23 +41,34 @@ const createProject = document.querySelector(`.newProject`)
 createProject.addEventListener(`click`, () => {
     const listOption = document.createElement(`option`)
     listOption.value = projectName.value
-    for(let i = 0; i < projects.length; i++){
-    listOption.setAttribute(`index`,`${i}`)
-    }
     projectList.appendChild(listOption)
+    const listOptions = document.querySelectorAll(`option`)
+    for(let i = 0; i < projects.length; i++){
+    listOptions.forEach((element => {element.setAttribute(`index`,`${i++}`)}))
+    }
     projects.push([])
-    console.log(projects)
-    const viewProjects = document.querySelector(`.allProjects`)
+})
+const viewProjects = document.querySelector(`.allProjects`)
 viewProjects.addEventListener(`click`, () => {
     content.innerHTML = ""
     const allOptions = document.querySelectorAll(`option`)
-    for(let i = 0; i < projects.length - 1; i++){
+    for(let i = 0; i < projects.length; i++){
         const projectDashboard = document.createElement(`div`)
-        projectDashboard.textContent = listOption.getAttribute(`value`)
+        projectDashboard.textContent = allOptions[i].getAttribute(`value`)
         content.appendChild(projectDashboard)
-        console.log(listOption)
+        console.log(allOptions)
     }   
 })
+const editProject = document.querySelector(`.editProject`)
+editProject.addEventListener(`click`, () =>{
+    content.innerHTML = ""
+    const listOption = document.querySelector(`.projectInput`)
+    const listOptions = document.querySelectorAll(`option`)
+    const index = listOptions.forEach(element => {
+        if(element.getAttribute(`value`) == listOption.value){
+            return element.getAttribute(`index`)
+        }
+    });
+    console.log(index)
 })
-
 export {createBtn, loadToDos}
